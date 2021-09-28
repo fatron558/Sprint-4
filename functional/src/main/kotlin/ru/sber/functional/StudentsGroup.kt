7d8b666facfecad030b20b86105a01bad5880994
Alexey <AAnaKireev@sberbank.ru>
2021-09-28 21:35:57 +0300

    готово

--- functional/src/main/kotlin/ru/sber/functional/StudentsGroup.kt
+++ functional/src/main/kotlin/ru/sber/functional/StudentsGroup.kt
@@ -4,7 +4,21 @@ class StudentsGroup {
 
     lateinit var students: List<Student>
 
-    fun filterByPredicate() {
-        TODO()
+    fun filterByPredicate(predicate: (Student) -> Boolean): List<Student> = students.filter { predicate(it) }
+
+    init {
+        var listStudents = ArrayList<Student>()
+        for (i in 0..5) {
+            listStudents.add(Student("firstName$i", "lastName$i",
+                "middleName$i", i, i.toDouble() * 2,
+                "city$i", "specialization$i", "prevEducation$i"))
+        }
+        students = listStudents
     }
+}
+
+fun main() {
+    val group = StudentsGroup().filterByPredicate { it.age >= 3 }
+    for (student in group)
+        println("${student.firstName} ${student.lastName} ${student.middleName} ${student.age}")
 }
