d65181baee2f9174b2510f89a235e4ebb872ca1b
Alexey <AAnaKireev@sberbank.ru>
2021-09-29 18:10:52 +0300

    готово

--- generics/src/main/kotlin/ru/sber/generics/Generics.kt
+++ generics/src/main/kotlin/ru/sber/generics/Generics.kt
@@ -1,27 +1,28 @@
 package ru.sber.generics
 
-import com.sun.org.apache.xpath.internal.operations.Bool
-import java.util.*
-
-// 1.
-//fun compare(p1: Pair, p2: Pair): Boolean {
-//    return false
-//}
-
-// 2.
-fun countGreaterThan(anArray: Array<Any>, elem: Any): Int {
-    return 0
+fun <A, B> compare(p1: Pair<A, B>, p2: Pair<A, B>): Boolean {
+    return p1.first == p2.first && p1.second == p2.second
 }
 
-// 3.
-class Sorter {
-//    val list: MutableList
-//
-//    fun add(value: Any) {
-//    }
+fun <E : Comparable<E>> countGreaterThan(anArray: Array<E>, elem: E): Int {
+    return anArray.filter { it > elem }.size
 }
 
-// 4.
-class Stack {
+class Sorter<T : Comparable<T>> {
+    val list: MutableList<T> = mutableListOf()
 
+    fun add(value: T) {
+        list.add(value)
+        list.sort()
+    }
+}
+
+class Stack<T> {
+    var stack = ArrayDeque<T>()
+
+    fun isEmpty(): Boolean = stack.isEmpty()
+
+    fun push(value: T) = stack.add(value)
+
+    fun pop(): T? = if (stack.isNotEmpty()) stack.removeLast() else null
 }
